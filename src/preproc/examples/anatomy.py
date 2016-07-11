46ca6cf4835728f086462321dc304e5055001090
pelednoam <peled.noam@gmail.com>
2016-07-11 16:32:01 -0400

    set the sftp as cmd args

--- src/preproc/examples/anatomy.py
+++ src/preproc/examples/anatomy.py
@@ -8,9 +8,9 @@ from src.utils import args_utils as au
 def get_subject_files_using_sftp(subject):
     args = anat.read_cmd_args(['-s', subject])
     args.sftp = True
-    args.sftp_username = 'npeled'
-    args.sftp_domain = 'door.nmr.mgh.harvard.edu'
-    args.remote_subject_dir = op.join('/autofs/cluster/neuromind/npeled/subjects', subject)
+    args.sftp_username = args.sftp_username
+    args.sftp_domain = args.sftp_domain
+    args.remote_subject_dir = op.join(args.remote_subject_dir, subject)
     anat.run_on_subjects(args)
 
 
@@ -30,6 +30,10 @@ def prepare_subject_folder_from_huygens(subject):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MMVT')
     parser.add_argument('-s', '--subject', help='subject name', required=True, type=au.str_arr_type)
+    parser.add_argument('-u', '--sftp_username', help='sftp username', required=False, default='npeled')
+    parser.add_argument('-d', '--sftp_domain', help='sftp domain', required=False, default='door.nmr.mgh.harvard.edu')
+    parser.add_argument('--remote_subject_dir', help='remote_subjects_dir', required=False,
+                        default='/autofs/cluster/neuromind/npeled/subjects')
     parser.add_argument('-f', '--function', help='function name', required=True)
     args = utils.Bag(au.parse_parser(parser))
     for subject in args.subject:
