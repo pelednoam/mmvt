7f0543b9cec5d79493a50e1b235dc8c1da764980
pelednoam <peled.noam@gmail.com>
2016-07-20 16:29:20 -0400

    create labels coloring from matlab file

---
+++ src/data_transformation/create_labels_coloring.py
@@ -0,0 +1,44 @@
+import scipy.io as sio
+import os.path as op
+import numpy as np
+
+from src.utils import utils
+from src.utils import labels_utils as lu
+
+LINKS_DIR = utils.get_links_dir()
+SUBJECTS_DIR = utils.get_link_dir(LINKS_DIR, 'subjects', 'SUBJECTS_DIR')
+MMVT_DIR = op.join(LINKS_DIR, 'mmvt')
+
+
+def create_coloring(x, subject, atlas, conditions, exclude=['unknown', 'corpuscallosum']):
+    labels = lu.read_labels(subject, SUBJECTS_DIR, atlas, exclude=exclude, sorted_according_to_annot_file=True,
+                            only_names=True)
+    for cond_id, cond_name in enumerate(conditions):
+        values = x[:, cond_id]
+        colors = utils.arr_to_colors(values, np.min(values), np.max(values), colors_map='YlOrRd')
+        coloring_fname = op.join(MMVT_DIR, subject, 'coloring', 'labels_{}_coloring.csv'.format(cond_name))
+        write_coloring_file(coloring_fname, labels, colors)
+    values_diff = np.squeeze(np.diff(x))
+    abs_max = max(map(abs, [np.max(values_diff), np.min(values_diff)]))
+    colors = utils.mat_to_colors(values_diff, -abs_max, abs_max, 'RdBu', flip_cm=True)
+    coloring_fname = op.join(MMVT_DIR, subject, 'coloring', 'labels_{}_{}_diff_coloring.csv'.format(*conditions))
+    write_coloring_file(coloring_fname, labels, colors)
+
+
+def write_coloring_file(coloring_fname, labels, colors):
+    with open(coloring_fname, 'w') as output_file:
+        for label_name, color_rgb in zip(labels, colors):
+            output_file.write('{},{},{},{}\n'.format(label_name, *color_rgb))
+
+
+if __name__ == '__main__':
+    subject = 'fsaverage5c'
+    fsaverage = 'fsaverage5c'
+    atlas = 'laus125'
+
+    root = '/cluster/neuromind/npeled/linda'
+    values_mat = sio.loadmat(op.join(root, 'figure_file2.mat'))['figure_file2']
+    conditions = ['task', 'rest']
+    exclude = ['unknown', 'corpuscallosum']
+    create_coloring(values_mat, subject, atlas, conditions, exclude)
+    print('finish!')
