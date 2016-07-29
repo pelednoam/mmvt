2280cc3613c8395f3b71885fb17a4da67e5aea8f
pelednoam <peled.noam@gmail.com>
2016-07-28 22:50:15 -0400

    fix some stuff

--- src/utils/movies_utils.py
+++ src/utils/movies_utils.py
@@ -2,6 +2,23 @@ import os.path as op
 import glob
 from src.utils import utils
 
+import moviepy.config as conf
+from moviepy.config import change_settings
+change_settings({"IMAGEMAGICK_BINARY": r"/usr/bin/convert"})
+
+# https://www.vultr.com/docs/install-imagemagick-on-centos-6
+# https://github.com/BVLC/caffe/issues/3884
+
+if conf.try_cmd([conf.FFMPEG_BINARY])[0]:
+    print("MoviePy : ffmpeg successfully found.")
+else:
+    print("MoviePy : can't find or access ffmpeg.")
+
+if conf.try_cmd([conf.IMAGEMAGICK_BINARY])[0]:
+    print("MoviePy : ImageMagick successfully found.")
+else:
+    print("MoviePy : can't find or access ImageMagick.")
+
 
 def cut_movie(movie_fol, movie_name, out_movie_name, subclips_times):
     from moviepy import editor
@@ -23,6 +40,8 @@ def crop_movie(movie_fol, movie_name, out_movie_name, crop_ys=(60, 1170)):
 
 
 def add_text_to_movie(movie_fol, movie_name, out_movie_name, subs):
+    # Should install ImageMagick
+    # For centos6: https://www.vultr.com/docs/install-imagemagick-on-centos-6
     from moviepy import editor
 
     def annotate(clip, txt, txt_color='red', fontsize=50, font='Xolonium-Bold'):
@@ -42,19 +61,6 @@ def create_animated_gif(movie_fol, movie_name, out_movie_name):
     from moviepy import editor
     video = editor.VideoFileClip(op.join(movie_fol, movie_name))
     video.write_gif(op.join(movie_fol, out_movie_name), fps=12)
-
-
-def edit_movie_example():
-    movie_fol = '/cluster/neuromind/npeled/videos/recordmydesktop'
-    movie_fol = '/cluster/neuromind/npeled/Documents/brain-map'
-    # cut_movie(movie_fol, 'out-7.ogv', 'freeview-mmvt.mp4')
-    # crop_movie(movie_fol, 'freeview-mmvt.mp4', 'freeview-mmvt_crop.mp4')
-    subs = [((0, 4), 'Clicking on the OFC activation in Freeview'),
-            ((4, 9), 'The cursor moved to the same coordinates in the MMVT'),
-            ((9, 12), 'Finding the closest activation in the coordinates'),
-            ((12, 16), 'The activation is displayed with its statistics')]
-    # add_text_to_movie(movie_fol, 'freeview-mmvt_crop.mp4', 'freeview-mmvt_crop_text.mp4', subs)
-    create_animated_gif(movie_fol, 'mg78_elecs_coh_meg_diff.mp4', 'mg78_elecs_coh_meg_diff.gif')
 
 
 def combine_movies(fol, movie_name, movie_type='mp4'):
@@ -79,12 +85,43 @@ def combine_movies(fol, movie_name, movie_type='mp4'):
         utils.remove_file('{}.avi'.format(part_name))
 
 
+def edit_movie_example():
+    movie_fol = '/cluster/neuromind/npeled/videos/recordmydesktop'
+    movie_fol = '/cluster/neuromind/npeled/Documents/brain-map'
+    # cut_movie(movie_fol, 'out-7.ogv', 'freeview-mmvt.mp4')
+    # crop_movie(movie_fol, 'freeview-mmvt.mp4', 'freeview-mmvt_crop.mp4')
+    subs = [((0, 4), 'Clicking on the OFC activation in Freeview'),
+            ((4, 9), 'The cursor moved to the same coordinates in the MMVT'),
+            ((9, 12), 'Finding the closest activation in the coordinates'),
+            ((12, 16), 'The activation is displayed with its statistics')]
+    # add_text_to_movie(movie_fol, 'freeview-mmvt_crop.mp4', 'freeview-mmvt_crop_text.mp4', subs)
+    create_animated_gif(movie_fol, 'mg78_elecs_coh_meg_diff.mp4', 'mg78_elecs_coh_meg_diff.gif')
 
 
+def edit_movie_example2():
+    movie_fol = '/home/noam/Videos/mmvt'
+    if not op.isdir(movie_fol):
+        movie_fol = '/autofs/cluster/neuromind/npeled/videos'
+    subclips_times = [(2, 57)]
+    # cut_movie(movie_fol, 'out-13.ogv', 'freeview-mmvt-electrodes.mp4', subclips_times)
+    # crop_movie(movie_fol, 'freeview-mmvt-electrodes.mp4', 'freeview-mmvt-electrodes_crop.mp4')
+    subs = [((0, 4), 'The brain is a 3d object imported from FreeSurfer'),
+            ((4, 18), 'To see the depth electrodes, the user can hide the hemispheres'),
+            ((18, 30), 'The user can choose an electrode and see it location both in MMVT and FreeView'),
+            ((30, 35), "Also, the user can choose to see only the current electrodes' lead"),
+            ((35, 40), "The program estimates the each electrode's sources and plot the probailites as colors\n" +
+             "Yellow: low probability, red: high probability"),
+            ((40, 55), "Chagning to a different lead")]
+    add_text_to_movie(movie_fol, 'freeview-mmvt-electrodes.mp4', 'freeview-mmvt-electrodes_sub.mp4', subs)
+    # create_animated_gif(movie_fol, 'mg78_elecs_coh_meg_diff.mp4', 'mg78_elecs_coh_meg_diff.gif')
 
-if __name__ == '__main__':
+
+def edit_movie_example3():
     movie_fol = '/home/noam/Pictures/mmvt/mg99/lvf4-3_4_1'
     movie_name = 'mg99_LVF4-3_stim_srouces_long.mp4'
     out_movie_name = 'mg99_LVF4-3_stim_srouces.mp4'
     subclips_times = [(0, 29)]
     cut_movie(movie_fol, movie_name, out_movie_name, subclips_times)
+
+if __name__ == '__main__':
+    edit_movie_example2()
