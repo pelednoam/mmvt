efc69dd463972dcdf39d06a6fed81c8ab83242bb
pelednoam <peled.noam@gmail.com>
2016-08-01 15:59:04 -0400

    Example 3

--- src/examples/movies.py
+++ src/examples/movies.py
@@ -35,23 +35,23 @@ def edit_movie_example2():
 
 
 def edit_movie_example3():
-    movie_fol = '/home/noam/Videos/mmvt'
+    movie_fol = '/home/noam/Desktop'
     if not op.isdir(movie_fol):
         movie_fol = '/cluster/neuromind/npeled/videos/mmvt/mmvt-meg-fmri-electrodes2'
-    subclips_times = [(2, 17), (21, 30), (36, 59)]
-    mu.cut_movie(movie_fol, 'out-18.ogv', 'mmvt-meg-fmri-electrodes.mp4', subclips_times)
-    mu.crop_movie(movie_fol, 'mmvt-meg-fmri-electrodes.mp4', 'mmvt-meg-fmri-electrodes_crop.mp4')
+    subclips_times = [(2, 39)]
+    # mu.cut_movie(movie_fol, 'f4580000-2784.avi', 'mmvt-meg-fmri-electrodes.mp4', subclips_times)
+    # mu.crop_movie(movie_fol, 'mmvt-meg-fmri-electrodes.mp4', 'mmvt-meg-fmri-electrodes_crop.mp4')
     subs = [((0, 5), 'The brain is a 3D object'),
-            ((5, 8), 'Adding the invasive electrodes'),
-            ((8, 11), ' '),
+            ((5, 9), 'Adding the invasive electrodes'),
+            ((9, 11), ' '),
             ((11, 14), "Selecting a time point from all the cortical labels' evoked responses"),
             ((15, 20), "Plotting the MEG for the selected time point"),
-            ((20, 29  ), "Choosing and plotting the fMRI constrast"),
-            ((29, 32), " "),
-            ((32, 41), "Selecting a time point from an electrode's evoked response"),
-            ((41, 47), "Plotting the electrodes' activity")]
-    mu.add_text_to_movie(movie_fol, 'mmvt-meg-fmri-electrodes_crop.mp4', 'mmvt-meg-fmri-electrodes_crop_sub.mp4', subs, fontsize=60)
-    mu.create_animated_gif(movie_fol, 'mg78_elecs_coh_meg_diff.mp4', 'mg78_elecs_coh_meg_diff.gif')
+            ((20, 26), "Choosing and plotting the fMRI constrast"),
+            ((26, 30), " "),
+            ((30, 32), "Selecting a time point from an electrode's evoked response"),
+            ((32, 35), "Plotting the electrodes' activity")]
+    mu.add_text_to_movie(movie_fol, 'mmvt-meg-fmri-electrodes.mp4', 'mmvt-meg-fmri-electrodes_sub.mp4', subs, fontsize=60)
+    # mu.create_animated_gif(movie_fol, 'mg78_elecs_coh_meg_diff.mp4', 'mg78_elecs_coh_meg_diff.gif')
 
 
 def edit_movie_example4():
