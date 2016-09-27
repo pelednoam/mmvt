44063bbdb244ae7c77c154b5fb55888ec17f78ae
pelednoam <peled.noam@gmail.com>
2016-09-27 13:04:48 -0400

    importing STN

--- src/misc/non_freesurfer_atlas.py
+++ src/misc/non_freesurfer_atlas.py
@@ -1,12 +1,20 @@
 import os.path as op
 import nibabel as nib
 import numpy as np
-import mne
+import shutil
+import os
 
 from src.utils import utils
 
 LINKS_DIR = utils.get_links_dir()
+SUBJECTS_DIR = utils.get_link_dir(LINKS_DIR, 'subjects', 'SUBJECTS_DIR')
 MMVT_DIR = op.join(LINKS_DIR, 'mmvt')
+
+
+MRI_PRETESS = 'mri_pretess {hemi}/{region}_mask.nii.gz 1 {colin_norm_fname} tmp/{region}_{hemi}_filled.mgz'
+MRI_TESSELLATE = 'mri_tessellate {hemi}/{region}_mask.nii.gz 1 tmp/{region}_{hemi}_notsmooth'
+MRIS_SMOOTH = 'mris_smooth -nw tmp/{region}_{hemi}_notsmooth tmp/{region}_{hemi}_smooth'
+MRIS_CONVERT = 'mris_convert tmp/{region}_{hemi}_smooth tmp/{region}_{hemi}.asc'
 
 
 def prepare_mask_file(region_fname, overwrite=False):
@@ -24,28 +32,31 @@ def prepare_mask_file(region_fname, overwrite=False):
         nib.save(mask, output_fname)
 
 
-def to_ras(points):
-    points = apply_trans(np.linalg.inv(org_vox2ras_tkr()), points)
-    points = apply_trans(vox2ras_tkr(), points)
+def to_ras(points, org_vox2ras_tkr, shifter_vox2tk_ras):
+    points = apply_trans(np.linalg.inv(org_vox2ras_tkr), points)
+    points = apply_trans(shifter_vox2tk_ras, points)
     return points
 
 
 def apply_trans(trans, points):
     return np.array([np.dot(trans, np.append(p, 1))[:3] for p in points])
 
-# mri_info --vox2ras-tkr rh/Striatum_mask.mgz
-def org_vox2ras_tkr():
-    return np.array(
-        [[-0.300, 0.000, 0.000, 25.200],
-        [0.000, 0.000, 0.300, -29.700],
-        [0.000, -0.300, 0.000, 37.200],
-        [0.000, 0.000, 0.000, 1.000]])
 
-    # return np.array(
-    #     [[-0.300, 0.000, 0.000, 25.200],
-    #     [0.000, 0.000, 0.300, -29.700],
-    #     [0.000, -0.300, 0.000, 37.200],
-    #     [0.000, 0.000, 0.000, 1.000]])
+# def org_vox2ras_tkr():
+#     # mri_info --vox2ras-tkr rh/Striatum_mask.mgz
+#     return np.array(
+#         [[-0.300, 0.000, 0.000, 25.200],
+#         [0.000, 0.000, 0.300, -29.700],
+#         [0.000, -0.300, 0.000, 37.200],
+#         [0.000, 0.000, 0.000, 1.000]])
+
+
+def get_shifted_vox2tk_ras(zero_in_T1_tk_ras, voxels_sizes):
+    return np.array(
+        [[-voxels_sizes[0], 0.0, 0.0, zero_in_T1_tk_ras[0]],
+         [0.0, voxels_sizes[1], 0.0, zero_in_T1_tk_ras[1]],
+         [0.0, 0.0, voxels_sizes[2], zero_in_T1_tk_ras[2]],
+         [0.0, 0.0, 0.0, 1.0]])
 
 
 def vox2ras_tkr():
@@ -69,19 +80,57 @@ def vox2ras_tkr():
          [0.000, 0.000, 0.000, 1.000]])
 
 
+def get_vox2ras(fname):
+    output = utils.run_script('mri_info --vox2ras {}'.format(fname))
+    return read_transform_matrix_from_output(output)
 
-def convert_to_ply(srf_fname, region_name, affine_trans=None):
-    ply_fname = op.join(get_fol_name(srf_fname), '{}.ply'.format(region_name))
+
+def get_vox2ras_tkr(fname):
+    output = utils.run_script('mri_info --vox2ras-tkr {}'.format(fname))
+    return read_transform_matrix_from_output(output)
+
+
+def ras_to_tkr_ras(fname):
+    ras2vox = np.linalg.inv(get_vox2ras(fname))
+    vox2tkras = get_vox2ras_tkr(fname)
+    return np.dot(ras2vox, vox2tkras)
+
+
+def calc_shifted_vox2tk_ras(region_fname, colin_T1_fname):
+    region_vox2ras = get_vox2ras(region_fname)
+    zero_in_ras = apply_trans(region_vox2ras, [[0, 0, 0]])
+    zero_in_T1_voxels = apply_trans(np.linalg.inv(get_vox2ras(colin_T1_fname)), zero_in_ras)
+    zero_in_T1_tk_ras = apply_trans(get_vox2ras_tkr(colin_T1_fname), zero_in_T1_voxels)
+    voxels_sizes = [abs(v) for v in np.diag(region_vox2ras)[:-1]]
+    shifted_vox2tk_ras = get_shifted_vox2tk_ras(zero_in_T1_tk_ras[0], voxels_sizes)
+    return shifted_vox2tk_ras
+
+
+def read_transform_matrix_from_output(output):
+    import re
+    str_mat = output.decode('ascii').split('\n')
+    for i in range(len(str_mat)):
+        str_mat[i] = re.findall(r'[+-]?[0-9.]+', str_mat[i])
+    del str_mat[-1]
+    return np.array(str_mat).astype(float)
+
+
+def mask_to_srf(atlas_fol, region, hemi, colin_norm_fname):
+    os.chdir(atlas_fol)
+    rs = utils.partial_run_script(locals())
+    # rs(MRI_PRETESS)
+    rs(MRI_TESSELLATE)
+    rs(MRIS_SMOOTH)
+    rs(MRIS_CONVERT)
+    shutil.move(op.join(atlas_fol, 'tmp', '{}_{}.asc'.format(region, hemi)),
+                op.join(atlas_fol, 'tmp', '{}_{}.srf'.format(region, hemi)))
+
+
+def convert_to_ply(srf_fname, ply_fname, org_vox2ras_tkr, shifter_vox2tk_ras):
     verts, faces, verts_num, faces_num = utils.read_srf_file(srf_fname)
-    verts = to_ras(verts)
-    # verts[:, 0] -= 44
-    # verts[:, 1] -= 4
-    # verts[:, 2] -= 29
-    # if affine_trans:
-    #     new_verts = mne.transforms.apply_trans(affine_trans, verts)
-    # verts[:, [1, 2]] = verts[:, [2, 1]]
+    verts = apply_trans(np.linalg.inv(org_vox2ras_tkr), verts)
+    verts = apply_trans(shifter_vox2tk_ras, verts)
     utils.write_ply_file(verts, faces, ply_fname)
-    # utils.srf2ply(srf_fname, )
 
 
 def get_fol_name(fname):
@@ -98,10 +147,21 @@ def get_namebase(fname):
         raise Exception('Unknown image type!')
 
 
-if __name__ == '__main__':
-    atals_fol = op.join(MMVT_DIR, 'maps', 'ATAG_Nonlinear_Keuken_2014')
+def main(region, atlas_fol, colin_T1_fname, overwrite=False):
     for hemi in ['lh', 'rh']:
-        region_fname = op.join(atals_fol, hemi, 'Striatum.nii.gz')
-        # prepare_mask_file(region_fname, overwrite=True)
-    convert_to_ply(op.join(atals_fol, 'tmp', 'Striatum_rh_notsmooth.srf'), 'striatum_rh')
+        region_fname = op.join(atlas_fol, hemi, '{}.nii.gz'.format(region))
+        prepare_mask_file(region_fname, overwrite=overwrite)
+        mask_to_srf(atlas_fol, region, hemi, op.join(SUBJECTS_DIR, 'colin27', 'mri', 'norm.mgz'))
+        region_vox2ras_tkr = get_vox2ras_tkr(region_fname)
+        shifted_vox2tk_ras = calc_shifted_vox2tk_ras(region_fname, colin_T1_fname)
+        convert_to_ply(op.join(atlas_fol, 'tmp', '{}_{}.srf'.format(region, hemi)),
+                       op.join(atlas_fol, 'tmp', '{}_{}.ply'.format(region, hemi)),
+                       region_vox2ras_tkr, shifted_vox2tk_ras)
+
+
+if __name__ == '__main__':
+    region =  'STN' #'Striatum'
+    atlas_fol = op.join(MMVT_DIR, 'maps', 'ATAG_Nonlinear_Keuken_2014')
+    colin_T1_fname = op.join(SUBJECTS_DIR, 'colin27', 'mri', 'T1.mgz')
+    main(region, atlas_fol, colin_T1_fname)
     print('Finish!')
