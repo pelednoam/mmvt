fbb8cfed3e8dae2efeb79075d1a00016a049005c
pelednoam <peled.noam@gmail.com>
2016-12-12 15:56:51 -0500

    using preproc utils in preprocs

--- src/preproc/stim.py
+++ src/preproc/stim.py
@@ -7,7 +7,6 @@ from src.utils import labels_utils as lu
 
 LINKS_DIR = utils.get_links_dir()
 SUBJECTS_DIR = utils.get_link_dir(LINKS_DIR, 'subjects', 'SUBJECTS_DIR')
-FREE_SURFER_HOME = utils.get_link_dir(LINKS_DIR, 'freesurfer', 'FREESURFER_HOME')
 BLENDER_ROOT_DIR = op.join(LINKS_DIR, 'mmvt')
 
 
