de1cc942fc3b7d1dc000a0b9d995ab8cdb14c78b
pelednoam <peled.noam@gmail.com>
2017-12-14 13:41:15 -0500

    mri_cvs_register

--- src/misc/transfer_electrodes_to_template.py
+++ src/misc/transfer_electrodes_to_template.py
@@ -29,7 +29,7 @@ def robust_register_to_template(subjects, template_system, subjects_dir, vox2vox
 
 def cvs_register_to_template(subjects, template_system, subjects_dir, print_only=False):
     subject_to = 'fsaverage5' if template_system == 'ras' else 'colin27' if template_system == 'mni' else template_system
-    for subject_from in subjects:
+    for subject_from in ['mg90']:# subjects:
         utils.delete_folder_files(op.join(subjects_dir, subject_from, 'mri_cvs_register_to_{}'.format(subject_to)))
         cmd = mri_cvs_register
         rs = utils.partial_run_script(locals(), print_only=print_only)
@@ -310,7 +310,7 @@ if __name__ == '__main__':
 
     electrodes = read_csv_file(op.join(root, csv_name), save_as_bipolar)
     # print(','.join(electrodes.keys()))
-    # cvs_register_to_template(electrodes.keys(), template_system, SUBJECTS_DIR)
+    cvs_register_to_template(electrodes.keys(), template_system, SUBJECTS_DIR)
     # template_electrodes = transfer_electrodes_to_template_system(electrodes, template_system)
     # save_template_electrodes_to_template(template_electrodes, save_as_bipolar, template_system, 'stim_')
     # export_into_csv(template_electrodes, template_system, 'stim_')
