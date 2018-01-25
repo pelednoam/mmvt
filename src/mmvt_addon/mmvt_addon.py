60266603e6d0366a704d4cf5a2d6118f2c9d24d7
ohadfel@github.com <ohadfel@gmail.com>
2018-01-25 11:12:02 +0200

    Add panel for Moshe Abeles. Fix for slicer panel.

--- src/mmvt_addon/mmvt_addon.py
+++ src/mmvt_addon/mmvt_addon.py
@@ -82,6 +82,8 @@ import skull_panel
 importlib.reload(skull_panel)
 import dell_panel
 importlib.reload(dell_panel)
+import moshes_panel
+importlib.reload(moshes_panel)
 
 
 print("mmvt addon started!")
@@ -563,7 +565,7 @@ def get_panels(first_time=False):
     panels = [appearance_panel, show_hide_panel, selection_panel, coloring_panel, colorbar_panel, play_panel, filter_panel,
             render_panel, freeview_panel, transparency_panel, data_panel, where_am_i_panel, search_panel, load_results_panel,
             electrodes_panel, streaming_panel, stim_panel, fMRI_panel, meg_panel, connections_panel, vertex_data_panel, dti_panel,
-            slicer_panel, skull_panel, pizco_panel]#, dell_panel)
+            slicer_panel, skull_panel, pizco_panel, moshes_panel]#, dell_panel)
     if not mmvt_utils.IS_WINDOWS and not first_time:
         panels.append(dell_panel)
     return panels
