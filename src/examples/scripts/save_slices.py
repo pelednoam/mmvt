0853e23676ec0ac97b5c22b148435ae3475ac79d
pelednoam <peled.noam@gmail.com>
2018-04-18 16:38:22 -0400

    save slices

---
+++ src/examples/scripts/save_slices.py
@@ -0,0 +1,18 @@
+import bpy
+import os.path as op
+
+
+def run(mmvt):
+    screen = bpy.data.screens['Neuro']
+    perspectives = ['sagital', 'coronal', 'axial']
+    images_names = ['{}.png'.format(pres) for pres in perspectives]
+    ind = 0
+    for area in screen.areas:
+        if area.type == 'IMAGE_EDITOR':
+            override = bpy.context.copy()
+            override['area'] = area
+            override['screen'] = screen
+            image = bpy.data.images[images_names[ind]]
+            area.spaces.active.image = image
+            bpy.ops.image.save_as(override, filepath=op.join(mmvt.utils.get_user_fol(), 'figures', images_names[ind]))
+            ind += 1
