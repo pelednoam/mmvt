b5c010bfe8bb42f50ddb149a1952923e22242111
pelednoam <peled.noam@gmail.com>
2018-06-12 11:45:34 -0400

    random numbers starts from 1

--- src/examples/scripts/plot_vertices_example.py
+++ src/examples/scripts/plot_vertices_example.py
@@ -5,8 +5,7 @@ import numpy as np
 def run(mmvt):
     import importlib
     mu = mmvt.utils
-    scripts_fol = mu.get_current_fol()
-    mu.add_fol_to_path(scripts_fol)
+    mu.add_fol_to_path(mu.get_current_fol())
     import plot_vertices
     importlib.reload(plot_vertices)
 
@@ -16,12 +15,12 @@ def run(mmvt):
         verts = np.load(op.join(mu.get_user_fol(), 'surf', '{}.pial.npz'.format(hemi)))['verts']
         selected_verts = np.random.randint(0, len(verts), N)
         data[hemi] = np.vstack(
-            (selected_verts, np.random.randint(0, 10, len(selected_verts)))).T
+            (selected_verts, np.random.randint(1, 11, len(selected_verts)))).T
     for hemi in ['Left', 'Right']:
         verts = np.load(op.join(mu.get_user_fol(), 'subcortical', 'Left-Cerebellum-Cortex.npz'))['verts']
         selected_verts = np.random.randint(0, len(verts), N)
         data['{}-Cerebellum-Cortex'.format(hemi)] = np.vstack(
-            (selected_verts, np.random.randint(0, 10, len(selected_verts)))).T
+            (selected_verts, np.random.randint(1, 11, len(selected_verts)))).T
     fol = mu.make_dir(op.join(mu.get_user_fol(), 'vertices_data'))
     np.savez(op.join(fol, 'vertices_data.npz'), **data)
     plot_vertices.run(mmvt)
