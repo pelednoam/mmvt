6797b7a182e10b3e16c589a223893471304b52f2
pelednoam <peled.noam@gmail.com>
2018-08-30 21:11:06 -0400

    PSD

---
+++ src/misc/power_spectral_density.py
@@ -0,0 +1,89 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import mne
+from mne.datasets import sample
+from mne.minimum_norm import read_inverse_operator, compute_source_psd_epochs
+
+print(__doc__)
+
+data_path = sample.data_path()
+fname_inv = data_path + '/MEG/sample/sample_audvis-meg-oct-6-meg-inv.fif'
+fname_raw = data_path + '/MEG/sample/sample_audvis_raw.fif'
+fname_event = data_path + '/MEG/sample/sample_audvis_raw-eve.fif'
+label_name = 'Aud-lh'
+fname_label = data_path + '/MEG/sample/labels/%s.label' % label_name
+
+event_id, tmin, tmax = 1, -0.2, 0.5
+snr = 1.0  # use smaller SNR for raw data
+lambda2 = 1.0 / snr ** 2
+method = "dSPM"  # use dSPM method (could also be MNE or sLORETA)
+em = 'mean_flip'
+
+# Load data
+inverse_operator = read_inverse_operator(fname_inv)
+label = mne.read_label(fname_label)
+raw = mne.io.read_raw_fif(fname_raw)
+events = mne.read_events(fname_event)
+
+# Set up pick list
+include = []
+raw.info['bads'] += ['EEG 053']  # bads + 1 more
+
+# pick MEG channels
+picks = mne.pick_types(raw.info, meg=True, eeg=False, stim=False, eog=True,
+                       include=include, exclude='bads')
+# Read epochs
+epochs = mne.Epochs(raw, events, event_id, tmin, tmax, picks=picks,
+                    baseline=(None, 0), reject=dict(mag=4e-12, grad=4000e-13, eog=150e-6))
+
+# define frequencies of interest
+fmin, fmax = 0., 70.
+bandwidth = 4.  # bandwidth of the windows in Hz
+
+# compute source space psd in label
+stcs = compute_source_psd_epochs(epochs, inverse_operator, lambda2=lambda2,
+                                 method=method, fmin=fmin, fmax=fmax,
+                                 bandwidth=bandwidth, label=label,
+                                 return_generator=True)
+
+# compute average PSD over the first 10 epochs
+n_epochs = 10
+psd_avg = 0
+for i, stc in enumerate(stcs):
+    if i >= n_epochs:
+        break
+    psd_avg += np.mean(stc.data, axis=0)
+
+psd_avg /= n_epochs
+psd_avg = 10 * np.log10(psd_avg)
+
+freqs = stc.times  # the frequencies are stored here
+
+plt.figure()
+plt.plot(freqs, psd_avg)
+plt.xlabel('Freq (Hz)')
+plt.ylabel('Power Spectral Density')
+plt.title('compute_source_psd_epochs')
+
+stc = mne.minimum_norm.apply_inverse_epochs(epochs, inverse_operator, lambda2, method, return_generator=True)
+label_ts = mne.extract_label_time_course(stc, label, inverse_operator['src'], mode=em, return_generator=True)
+psd_avg = 0
+for epoch_ind, label_ts in enumerate(label_ts):
+    if epoch_ind >= n_epochs:
+        break
+    psds, freqs = mne.time_frequency.psd_array_multitaper(
+        label_ts, epochs.info['sfreq'], fmin, fmax, bandwidth)
+    psd_avg += psds.squeeze()
+
+psd_avg /= n_epochs
+psd_avg = 10 * np.log10(psd_avg)
+
+
+plt.figure()
+plt.plot(freqs, psd_avg)
+plt.xlabel('Freq (Hz)')
+plt.ylabel('Power Spectral Density')
+plt.title('psd_array_multitaper on label_ts')
+
+plt.show()
