0f7179a8d6c4910c63746fd928ec723106b79b5f
Noam Peled <peled.noam@gmail.com>
2018-09-12 12:24:55 -0400

    slicing

--- src/examples/scripts/slicing_movie.py
+++ src/examples/scripts/slicing_movie.py
@@ -1,6 +1,7 @@
 import bpy
 import numpy as np
 import os.path as op
+import time
 
 
 def run(mmvt):
@@ -18,21 +19,31 @@ def run(mmvt):
     mmvt.show_hide.show_hemis()
     mmvt.show_hide.show_subcorticals()
     mmvt.show_hide.show_coronal(show_frontal=True)
+    mmvt.show_hide.show_head()
+
+    mmvt.transparency.set_brain_transparency(0)
+    mmvt.transparency.set_head_transparency(1)
 
     coordinates = mu.get_cursor_location()
     bpy.context.scene.slicing_movie_from_y = coordinates[1]
     mmvt.where_am_i_panel.create_slices()
     mmvt.slicer.set_slices_plot_cross(False)
     mmvt.slicer.set_slicer_cut_type(mmvt.slicer.CUT_CORONAL)
-    for y in np.arange(bpy.context.scene.slicing_movie_from_y, bpy.context.scene.slicing_movie_to_y,
-                       bpy.context.scene.slicing_movie_dy):
+    slicing_range = np.arange(bpy.context.scene.slicing_movie_from_y, bpy.context.scene.slicing_movie_to_y,
+                              bpy.context.scene.slicing_movie_dy)
+    now, N = time.time(), len(slicing_range)
+    for run, y in enumerate(slicing_range):
+        mu.time_to_go(now, run, N, 1, do_write_to_stderr=True)
         cut_pos = [0, y, 0]
         coordinates[1] = y
-        mmvt.slicer.slice_brain(cut_pos, save_image=bpy.context.scene.slicing_save_movie,
-                                render_image=bpy.context.scene.slicing_render_movie)
+        mmvt.slicer.slice_brain(cut_pos, save_image=bpy.context.scene.slicing_save_movie, render_image=False)
+        if bpy.context.scene.slicing_render_movie:
+            mmvt.render.render_image('slicing_{}.{}'.format(
+                run, mmvt.render.get_figure_format()), set_to_camera_mode=True)
         mmvt.slicer.clear_slice()
         mmvt.where_am_i_panel.create_slices(pos=coordinates)
         return
+
 
 bpy.types.Scene.slicing_movie_from_y = bpy.props.FloatProperty(default=8)
 bpy.types.Scene.slicing_movie_to_y = bpy.props.FloatProperty(default=-9.2)
@@ -45,7 +56,7 @@ def draw(self, context):
     layout = self.layout
     layout.prop(context.scene, 'slicing_movie_from_y', text='from')
     layout.prop(context.scene, 'slicing_movie_to_y', text='to')
-    layout.prop(context.scene, 'slicing_movie_dy', text='dz')
+    layout.prop(context.scene, 'slicing_movie_dy', text='dy')
     layout.prop(context.scene, 'slicing_render_movie', text='Render frames')
     layout.prop(context.scene, 'slicing_save_movie', text='Save frames')
 
