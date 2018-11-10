4020f27de1fb2ff4892451127229e5718844a62b
Alex Rockhill <alexrockhill@Alexs-MacBook-Pro.local>
2018-11-09 20:25:35 -0500

    Fixed rotation/reflection issues with MEG helmet

--- src/mmvt_addon/colors_utils.py
+++ src/mmvt_addon/colors_utils.py
@@ -293,10 +293,13 @@ def get_activity_map_im(im,cmap):
     ax.set_axis_off()
     fig.add_axes(ax)
     ax.imshow(im, cmap=cmap)
+    #x.invert_xaxis()
+    ax.invert_yaxis()
     map_dir = op.join(os.getcwd(), 'examples')
     fname = op.join(map_dir, 'activity_map.png')
     if op.isfile(fname):
         os.remove(fname)
+    print('Saving UV map to {}'.format(fname))
     fig.savefig(fname)
     plt.close(fig)
     img = bpy.data.images.load(fname)
